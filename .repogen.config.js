78f173cb7af77f333782d0f25386566786a3dff4
alechp <me@alechp.com>
2018-02-21 17:41:32 -0800

    Added symlinks from repogen, repogen config and gitignore

---
+++ .repogen.config.js
@@ -0,0 +1,20 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    {
+      servexyz: "auth0-starter"
+    },
+    {
+      servexyz: "cli-starter"
+    },
+    {
+      servexyz: "npm-starter"
+    },
+    {
+      servexyz: "service-starter"
+    }
+  ]
+};
+
+module.exports = { config };
