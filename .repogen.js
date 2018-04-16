1a70a9a18f722a4f57319322df82d15909daf459
alechp <me@alechp.com>
2018-04-16 16:22:21 -0700

    Removed react-webpack-starter

--- .repogen.js
+++ .repogen.js
@@ -2,22 +2,16 @@ const config = {
   provider: "alechp",
   repospacePath: __dirname,
   repositories: [
-    {
-      servexyz: "auth0-starter"
-    },
-    {
-      servexyz: "cli-starter"
-    },
-    {
-      servexyz: "npm-starter"
-    },
-    {
-      servexyz: "service-starter"
-    },
+    { servexyz: "auth0-starter" },
+    { servexyz: "cli-starter" },
+    { servexyz: "npm-starter" },
+    { servexyz: "service-starter" },
     { servexyz: "oauth-starter" },
     { servexyz: "ink-cli-starter" },
     { servexyz: "ant-amplify-react-starter" },
-    { servexyz: "amplify-react-starter" } 
+    { servexyz: "amplify-react-starter" },
+    { servexyz: "aws-sls-starter" },
+    { servexyz: "amplify-razzle-starter"}
   ]
 };
 
