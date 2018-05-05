c65663cd8c5ec0048e9d0fc5f96e47c5bdeb0850
alechp <me@alechp.com>
2018-05-05 12:13:30 -0700

    Updated repogen to incl aws-sls-auth-starter

--- .repogen.js
+++ .repogen.js
@@ -2,16 +2,14 @@ const config = {
   provider: "alechp",
   repospacePath: __dirname,
   repositories: [
-    { servexyz: "auth0-starter" },
     { servexyz: "cli-starter" },
     { servexyz: "npm-starter" },
     { servexyz: "service-starter" },
-    { servexyz: "oauth-starter" },
     { servexyz: "ink-cli-starter" },
     { servexyz: "ant-amplify-razzle-starter" },
     { servexyz: "amplify-razzle-starter"},
-    { servexyz: "aws-sls-starter" },
-    { servexyz: "amplify-react-starter" }
+    { servexyz: "semantic-amplify-razzle-starter" },
+    { servexyz: "aws-sls-auth-starter" }
   ]
 };
 
