910ece8dd0f27f441ab46982242340b385288c79
alechp <me@alechp.com>
2019-05-08 12:07:05 -0700

    Cleaned up repogen to active projects

--- .repogen.js
+++ .repogen.js
@@ -2,19 +2,11 @@ const config = {
   provider: "alechp",
   repospacePath: __dirname,
   repositories: [
-    { servexyz: "cli-starter" },
     { servexyz: "node-starter" },
-    { servexyz: "service-starter" },
-    { servexyz: "ink-cli-starter" },
-    { servexyz: "ant-amplify-razzle-starter" },
     { servexyz: "amplify-razzle-starter"},
-    { servexyz: "semantic-amplify-razzle-starter" },
     { servexyz: "aws-sls-auth-starter" },
     { servexyz: "prisma-starter" },
     { servexyz: "docsify-starter" },
-    { servexyz: "prisma-authorized-starter" },
-    { servexyz: "reach-amplify-unstated-razzle-starter" },
-    { servexyz: "razzle-amplify-unstated-starter" }
   ]
 };
 
